c09f617e2529e202d89e206c9e158aff904c5b3f
SJcodeML <sidrajabin56@gmail.com>
2024-04-07 21:04:11 -0700

    committed

---
+++ day 37/q 109/q91.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Question 91: Create an array of your three favorite fruits and add a new fruit to the end of the array.
+let favFruits = ["Mango", "Apple", "Pear"];
+favFruits.push("Guava");
+console.log(favFruits);
---
+++ day 37/q 109/q91.ts
@@ -0,0 +1,5 @@
+// Question 91: Create an array of your three favorite fruits and add a new fruit to the end of the array.
+let favFruits : string[]=["Mango","Apple","Pear"];
+
+favFruits.push("Guava");
+console.log(favFruits);
